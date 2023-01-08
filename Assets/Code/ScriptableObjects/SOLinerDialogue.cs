83c0e21c3c484c17cf06f94ad403882162e9c0d2
Karbovskyi Vladyslav <vkarbovskiy@qualium-systems.com>
2023-01-08 17:54:21 +0100

    so-added

---
+++ Assets/Code/ScriptableObjects/SOLinerDialogue.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Assets.Code.ScriptableObjects
+{
+    public class SOLinerDialogue : ScriptableObject
+    {
+        public string Name;
+        public string Sentence;
+        public SOBackgroundData BackgroundData;
+    }
+}
