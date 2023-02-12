7a18ee2e4d02ceb794e69a877e734cc8bdf76fd4
VladyslavKarbovskyi <vkarbovskiy@qualium-systems.com>
2023-02-12 13:58:59 +0100

    StreetBlockAlmostFinish

--- Assets/IntroActions.cs
+++ Assets/IntroActions.cs
@@ -9,11 +9,24 @@ public class IntroActions : MonoBehaviour
     [SerializeField] private AudioService _audioService;
     [SerializeField] private AudioClip _plasma;
     [SerializeField] private AudioClip _cough;
+    [SerializeField] private AudioClip _walking;
+    [SerializeField] private AudioClip _carAccident;
     [SerializeField] private Transform _examiner;
+    [SerializeField] private Transform _panelExaminer3Lines;
     [SerializeField] private Transform _mainCharacter;
+    [SerializeField] private Transform _panelMainCharacter3Lines;
+    [SerializeField] private Transform _panelStory;
+    [SerializeField] private Transform _walkingGirl;
+    [SerializeField] private Transform _car;
     
     [SerializeField] private Image _introBack;
     [SerializeField] private CanvasGroup _streetBack;
+
+    [SerializeField] private Image traficLightRed;
+    [SerializeField] private Image traficLightGreen;
+    private Color red=Color.red;
+    private Color green=Color.green;
+    private Color grey=Color.grey;
     
     
     private Vector3 _examinerPosition;
@@ -60,12 +73,47 @@ public class IntroActions : MonoBehaviour
     
     public void HideIntroAndShowStreet(StepFinishCallback s)
     {
-        _introBack.DOFade(0, 1).OnComplete(() => _streetBack.DOFade(1, 1).OnComplete(s.Invoke));
+        _introBack.DOFade(0, 1).OnComplete(() =>
+        {
+            SetPanelsPositionForStreet();
+            _streetBack.DOFade(1, 1).OnComplete(s.Invoke);
+        });
+    }
+
+    private void SetPanelsPositionForStreet()
+    {
+        _panelExaminer3Lines.DOLocalMove(new Vector3(398,151,0),0.1f);
+        _panelMainCharacter3Lines.DOLocalMove(new Vector3(-367,-387,0), 0.1f);
     }
     
     public void GirlMoveBehindTable(StepFinishCallback s)
     {
+        _audioService.PlayAudio(_walking);
+        _walkingGirl.DOLocalMove(new Vector3(-1120, -462, 0), 6f).OnComplete(() =>
+        {
+            _walkingGirl.eulerAngles=Vector3.zero;
+            _walkingGirl.localPosition = new Vector3(-2571, -562, 0);
+        });
+        _walkingGirl.DOScale(new Vector3(0.7f, 0.7f, 0.7f), 5f);
         s.Invoke();
+    }
+
+    public void MoveCar(StepFinishCallback s)
+    {
+        traficLightRed.DOColor(Color.gray, 1f).OnComplete(() =>
+        {
+            traficLightGreen.DOColor(Color.green, 1f).OnComplete((() =>
+            {
+                _walkingGirl.DOLocalMove(new Vector3(-2009, -456,0), 2);
+                _walkingGirl.DOScale(new Vector3(0.5f, 0.5f, 0.5f), 2);
+                _audioService.PlayAudio(_carAccident);
+                _car.DOLocalMove(new Vector3(-2290, -349, 0), 2).OnComplete(() =>
+                    {
+                        _walkingGirl.DOScale(Vector3.zero, 1).SetEase(Ease.InBounce);
+                        _car.DOScale(Vector3.zero, 1).SetEase(Ease.InBounce).OnComplete(s.Invoke);
+                    });
+            }));
+        });
     }
     
     
